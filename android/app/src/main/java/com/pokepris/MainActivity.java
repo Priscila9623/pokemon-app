ec3c03f5ba203339e44c08d3b31323ca85dcce37
Priscila <priscila.lopez18@itca.edu.sv>
2020-09-24 03:03:28 -0600

    :wrench: package display name and keystore

--- android/app/src/main/java/com/pokepris/MainActivity.java
+++ android/app/src/main/java/com/pokepris/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pokeapp;
+package com.pokepris;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/pokepris/MainApplication.java
+++ android/app/src/main/java/com/pokepris/MainApplication.java
@@ -1,4 +1,5 @@
-package com.pokeapp;
+
+package com.pokepris;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.pokeapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.pokepris.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
