4952921353501da0536651ea529fc8dffc6fefd3
Priscila <priscila.lopez18@itca.edu.sv>
2020-09-25 21:27:06 -0600

    :sparkles: handle showed screen

--- app/navigators/stackNavigator/index.js
+++ app/navigators/stackNavigator/index.js
@@ -2,37 +2,55 @@ import React, { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { transitionSpecConfig, transitionConfig } from '@helpers/screens/stackNavigatorConfig';
 import auth from '@react-native-firebase/auth';
+import SplashScreen from 'react-native-splash-screen';
 import userScreens from './userScreens';
 import authScreens from './authScreens';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
-  	const [user, setUser] = useState();
+	const [initializing, setInitializing] = useState(true);
+	const [user, setUser] = useState();
+	  
+	const onAuthStateChanged = (user) => {
+		setUser(user);
+		if (initializing) setInitializing(false);
+	};
+
 	useEffect(() => {
-		setUser(auth().currentUser);
+		if (!initializing) SplashScreen.hide();
+	}, [initializing])
+	
+	useEffect(() => {
+		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+		return subscriber;
 	}, []);
 
 	return (
-		<Stack.Navigator
-			initialRouteName={user ? 'Home' : 'Login'}
-			screenOptions={{
-				headerShown: false,
-				cardStyle: { backgroundColor: '#fff' },
-				gestureDirection: "horizontal",
-				transitionSpec:{
-					close: transitionSpecConfig,
-					open: transitionSpecConfig,
-				},
-				cardStyleInterpolator: (props) => transitionConfig(props),
-			}}
-		>
-			{Object.entries({
-				...(user ? userScreens : authScreens),
-			}).map(([name, component]) => (
-				<Stack.Screen key={name} name={name} component={component} />
-			))}
-		</Stack.Navigator>
+		<>
+			{!initializing && (
+				<Stack.Navigator
+					initialRouteName={user ? 'Register' : 'Login'}
+					screenOptions={{
+						headerShown: false,
+						// cardStyle: { backgroundColor: '#fff' },
+						gestureDirection: "horizontal",
+						transitionSpec:{
+							close: transitionSpecConfig,
+							open: transitionSpecConfig,
+						},
+						cardStyleInterpolator: (props) => transitionConfig(props),
+					}}
+				>
+					{Object.entries({
+						...(user ? userScreens : authScreens),
+					}).map(([name, component]) => (
+						<Stack.Screen key={name} name={name} component={component} />
+					))}
+				</Stack.Navigator>
+			)}
+		</>
+		
 	);
 };
 
