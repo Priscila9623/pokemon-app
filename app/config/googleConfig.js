2e237c3a49eb1a5af96c19f306fa760f065c5acb
Priscila <priscila.lopez18@itca.edu.sv>
2020-09-27 21:27:29 -0600

    :wrench: google sigin scopes

--- app/config/googleConfig.js
+++ app/config/googleConfig.js
@@ -1,9 +1,8 @@
 import { GoogleSignin } from 'react-native-google-signin';
-import Config from "react-native-config";
 
 const configSignin = ()=>{
 	GoogleSignin.configure({
-		scopes: ['https://www.googleapis.com/auth/drive.readonly'], webClientId: Config.WEB_CLIENT_ID,
+		scopes: ['https://www.googleapis.com/auth/drive.readonly'], webClientId: '369007017734-g92dk21hl01gvk7c5e4v13epl4pal46e.apps.googleusercontent.com',
 		offlineAccess: true,
 		hostedDomain: '',
 		loginHint: '',
